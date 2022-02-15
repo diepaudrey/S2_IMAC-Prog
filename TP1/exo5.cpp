fad2fdaee686c42019b1f57587e74000f16ce717
Audrey <diep.audrey@gmail.com>
2022-02-15 11:27:23 +0100

    Dossier prog steeve

---
+++ TP1/exo5.cpp
@@ -0,0 +1,27 @@
+#include "tp1.h"
+#include <QApplication>
+#include <time.h>
+
+int isMandelbrot(Point z, int n, Point point){
+    // recursiv Mandelbrot
+
+    // check n
+
+    // check length of z
+    // if Mandelbrot, return 1 or n (check the difference)
+    // otherwise, process the square of z and recall
+    // isMandebrot
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow* w = new MandelbrotWindow(isMandelbrot);
+    w->show();
+
+    a.exec();
+}
+
+
+
